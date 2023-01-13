eddeec7262c093e215eb400ec99834aeb2ada7c7
Nahuel Audisio <nahuel.audisio.95@gmail.com>
2023-01-13 12:20:43 -0300

    segundo commit

--- src/test/java/com/paltzi/javatest/util/StringUtilTest.java
+++ src/test/java/com/paltzi/javatest/util/StringUtilTest.java
@@ -5,15 +5,17 @@ package com.paltzi.javatest.util;
 public class StringUtilTest {
     public static void main(String[] args) {
 
-        String result = StringUtil.repeat("hola",3);
-        if (result.equals("holaholahola")){
-            System.out.println("OK");
-        }
+        assertEquals(StringUtil.repeat("hola",3),"holaholahola");
 
-        String result2 = StringUtil.repeat("hola",1);
-        if (result2.equals("hola")){
-            System.out.println("OK");
-        }
+
+        assertEquals(StringUtil.repeat("hola",1), "hola");
+
+    }
+
+    private static void assertEquals(String actual, String expected) {
+if (!actual.equals(expected)){
+    throw new RuntimeException(actual + " is not equal to expected " + expected);
+}
 
     }
 }
